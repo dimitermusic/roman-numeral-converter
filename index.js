1be58a6b611bc63353f95743b24098c2a64d12d4
dimitermusic <dimiteryyordanov@gmail.com>
2022-01-31 14:59:14 -0800

    updated logic for edge cases

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const romNumObj = {
     M: 1000,
 }
 
+// Convert string of valid Roman Numeral characters into integer.
 function romanNumConvert(str) {
 
     // Define variable that will accumulate Roman Numerals
@@ -27,13 +28,21 @@ function romanNumConvert(str) {
 
         // Use pointers to check if value to the right is greater and subtract current value if so
         if (romNumObj[currChar] < romNumObj[nextChar]) {
+
             sum += (romNumObj[nextChar] - romNumObj[currChar]);
             // Skip over this next value since already accounted for
             i++
+
+        } else if (romNumObj[currChar] >= romNumObj[nextChar]) {
+
             // If value to the right is greater, accumulate normally
-        } else if (romNumObj[currChar] > romNumObj[nextChar]) {
             sum += romNumObj[currChar]
-        }
+
+        } else
+
+            // Add the last character of the string to the sum
+            sum += romNumObj[currChar]
+
     }
 
     // Return final sum of string of Roman Numerals
@@ -41,10 +50,10 @@ function romanNumConvert(str) {
 
 }
 
-// preventDefault to prevent page from refreshing when button is clicked
-// Taking in user input from html page, plugging that into our multiplication function, and dynamically rendering the answer to the page.
+// Take user input, check if valid, plug into conversion function, render results to DOM.
 function handleConversionDisplay(e) {
 
+    // preventDefault to prevent page from refreshing when button is clicked
     e.preventDefault();
 
     const userInput = document.querySelector("#userInput").value;
@@ -60,7 +69,7 @@ function handleConversionDisplay(e) {
     } else {
         // If valid characters, convert using function and display to DOM
         ansEl.textContent = `${romanNumConvert(userInput)}`;
-        console.log(`Roman Numeral ${userInput} equals ${romanNumConvert(userInput)}`);
+        // console.log(`Roman Numeral ${userInput} equals ${romanNumConvert(userInput)}`);
     }
 }
 

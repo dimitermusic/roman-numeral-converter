0d29e819ed9c19edb33d26b7f7158e5ec78ae4c9
dimitermusic <dimiteryyordanov@gmail.com>
2022-03-03 12:38:11 -0800

    refactor js

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,27 +16,27 @@ const romNumObj = {
 // Convert string of valid Roman Numeral characters into integer.
 const romanNumConvert = (str) => {
 
-    // Define variable that will accumulate Roman Numerals
+    // Define variable that will accumulate Roman Numerals.
     let sum = 0
 
-    // Return numeric value of single character Roman Numeral
+    // Return numeric value of single character Roman Numeral.
     if (str.length === 1) return romNumObj[str];
 
-    // If more than a single character, loop over string to accumulate value
+    // If more than a single character, loop over string to accumulate value.
     for (let i = 0; i < str.length; i++) {
 
         // Define two pointers
         let currChar = str[i]
         let nextChar = str[i + 1]
 
-        // Use pointers to check if value to the right is greater and subtract current value if so
+        // Use pointers to check if value to the right is greater and subtract current value if so.
         if (romNumObj[currChar] < romNumObj[nextChar]) {
 
             sum += (romNumObj[nextChar] - romNumObj[currChar]);
-            // Skip over this next value since already accounted for
+            // Skip over this next value since already accounted for.
             i++
 
-            // If value to the right is greater or at the end of array, accumulate normally
+            // If value to the right is greater or at the end of array, accumulate normally.
         } else if ((romNumObj[currChar] >= romNumObj[nextChar]) || (!(romNumObj[nextChar]))) {
 
             sum += romNumObj[currChar]
@@ -45,7 +45,7 @@ const romanNumConvert = (str) => {
 
     }
 
-    // Return final sum of string of Roman Numerals
+    // Return final sum of string of Roman Numerals.
     return sum
 
 }
@@ -68,19 +68,19 @@ const handleCloseModal = () => {
 // Take user input, check if valid, plug into conversion function, render results to DOM.
 const handleConversionDisplay = (e) => {
 
-    // preventDefault to prevent page from refreshing when button is clicked
+    // preventDefault() to prevent page from refreshing when button is clicked.
     e.preventDefault();
 
-    // Select user input from text box on page
+    // Select user input from text box on page.
     const userInput = document.querySelector("#userInput").value.toUpperCase();
-    // Check for invalid characters and return message if any then refresh page
+    // Check for invalid characters and return message if any then refresh page.
     let invalidChars = userInput.split("").filter(char => !(char in romNumObj));
 
     if (invalidChars.length > 0 || userInput === "") {
         modal.style.display = "flex";
 
     } else {
-        // If valid characters, convert using function and display to DOM
+        // If valid characters, convert using function and display to DOM.
         ansEl.textContent = `${romanNumConvert(userInput)}`;
         console.log(`Roman Numeral ${userInput} equals ${romanNumConvert(userInput)}`);
     }
@@ -91,6 +91,7 @@ const handleConversionDisplay = (e) => {
 // Change page to dark mode with html switch.
 const handleDarkModeSwitch = () => {
 
+    // Select elements from DOM.
     const body = document.getElementById("body");
     const jumbotron = document.getElementById("jumbotron");
     const slider = document.getElementById("slider");
@@ -106,6 +107,7 @@ const handleDarkModeSwitch = () => {
 
         console.log("dark mode on");
 
+        // Add dark mode classes when switched on.
         body.classList.add("dark-mode-body")
         jumbotron.classList.add("dark-mode-jumbotron");
         slider.classList.add("dark-mode-slider");
@@ -123,6 +125,7 @@ const handleDarkModeSwitch = () => {
 
         console.log("dark mode off");
 
+        // Remove dark mode classes when switched off.
         body.classList.remove("dark-mode-body")
         jumbotron.classList.remove("dark-mode-jumbotron");
         slider.classList.remove("dark-mode-slider");
@@ -141,12 +144,16 @@ const handleDarkModeSwitch = () => {
 // If it is between 8pm and 7am local time, automatically switch on Dark Mode at page load.
 const handleDarkModeAuto = () => {
 
+    // Set range of hours to include in dark mode logic.
+    let darkPmHours = [8, 9, 10, 11]
+    let darkAmHours = [12, 1, 2, 3, 4, 5, 6]
+    // Current time.
     let hour = new Date().getHours();
     let minutes = new Date().getMinutes();
     let modifier = ""
     let darkModeOnOff = ""
 
-    // Time conversion from 24hr to 12hr format
+    // Time conversion from 24hr to 12hr format.
     if (hour > 12) {
         hour -= 12
         modifier = "PM"
@@ -159,13 +166,15 @@ const handleDarkModeAuto = () => {
         modifier = "AM"
     }
 
-    // Adding 0 to beginning of single digit minutes
+    // Adding 0 to beginning of single digit minutes.
     if (minutes.toString().length === 1) {
         minutes = `0${minutes}`
     }
 
-    // Dark mode automatically scheduled based on local time
-    if ((hour >= 8 && hour !== 12 && modifier === "PM") || ((hour <= 7 || hour === 12) && modifier === "AM")) {
+    console.log(typeof minutes);
+
+    // Dark mode automatically turns on based on set schedule.
+    if ((darkPmHours.includes(hour) && modifier === "PM") || (darkAmHours.includes(hour) && modifier === "AM")) {
         darkModeSwitch.checked = true
         handleDarkModeSwitch();
     } else {
@@ -173,11 +182,8 @@ const handleDarkModeAuto = () => {
         handleDarkModeSwitch();
     }
 
-    if (darkModeSwitch.checked) {
-        darkModeOnOff = "on"
-    } else {
-        darkModeOnOff = "off"
-    }
+    // Ternary operator to dynamically display if dark mode is on or off.
+    (darkModeSwitch.checked ? darkModeOnOff = "on" : darkModeOnOff = "off")
 
     console.log(`Dark Mode is currently ${darkModeOnOff} because the time is ${hour}:${minutes}${modifier}, and it is automatically scheduled to be on between 8pm and 7am`)
 
